e8321a73040361bf3b9a57021d883cca13ec488f
Puneet Aggarwal <aggarwal.puneet222@gmail.com>
2023-01-15 10:04:01 +0000

    Added postgres and docker file for authentication servi

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -6,9 +6,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const webPort = "80"
+const maxTries = 10
+const backoffTime = 2
 
 type Config struct {
 	DB     *sql.DB
@@ -19,9 +27,16 @@ func main() {
 	log.Println("Starting authentication service")
 
 	// TODO connect to DB
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Database connection failed!")
+	}
 
 	// set up config
-	app := Config{}
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -31,5 +46,43 @@ func main() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func openDBConnection(dsn string) (*sql.DB, error) {
+	conn, err := sql.Open("pgx", dsn)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		return nil, err
+	}
+
+	err = conn.Ping()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to ping to database: %v\n", err)
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+	tries := 0
+	for {
+		conn, err := openDBConnection(dsn)
+		if err == nil {
+			log.Printf("Connected to Postgres database!")
+			return conn
+		}
+		log.Printf("Postgres database not ready yet: %v\n", err)
+		tries++
+
+		if tries > maxTries {
+			fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+			return nil
+		}
+
+		time.Sleep(time.Second * backoffTime)
+		fmt.Printf("Backing off for %d seconds", backoffTime)
 	}
 }
